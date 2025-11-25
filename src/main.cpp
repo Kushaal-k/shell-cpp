8c120199d394b0e288bd780d9d21b12a869194ae
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-11-25 20:12:49 +0530

    [PATH]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+#include <filesystem>
+#include <unistd.h>
+
+namespace fs = std::filesystem;
+
+std::vector<std::string> builtIn = {"echo", "type", "exit"};
+
+bool isBuiltIn(std::vector<std::string>& builtin, std::string target){
+  for(auto& item: builtin){
+    if(item == target){
+      return true;
+    }
+  }
+
+  return false;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -11,7 +30,7 @@ int main() {
     std::cout << "$ ";
     
     std::getline(std::cin, command);
-    std::string builtIn[3] = {"echo", "type", "exit"};
+    
 
     if(command == "exit"){
       return 0;
@@ -22,13 +41,26 @@ int main() {
       continue;
     }
     else if(command.rfind("type", 0) == 0){
-      std::string text = command.substr(5,command.length());
+      std::string pathEnv = getenv("PATH");
+      std::stringstream ss(pathEnv);
+      std::string dir;
       bool found = false;
-      for(auto item : builtIn){
-        if(text == item){
-          std::cout << text << " is a shell builtin" << '\n';
-          found = true;
-          break;
+
+      std::string text = command.substr(5,command.length());
+      if(isBuiltIn(builtIn, text)){
+        std::cout << text << " is a shell builtin" << '\n';
+        continue;
+      }
+
+      while(getline(ss, dir, ':')){
+        fs::path fullPath = fs::path(dir)/text;
+
+        if(fs::exists(fullPath)){
+          if(access(fullPath.string().c_str(), X_OK) == 0){
+            std::cout << command << " is " << fullPath << std::endl;
+            found = true;
+            break;
+          }
         }
       }
       if(!found){
