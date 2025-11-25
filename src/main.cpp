b1a7873e0b5ca1d35beac767eef4b87c757d3a28
Kushaal Kankane <kushaalkankane12345@gmail.com>
2025-11-25 18:21:27 +0530

    [type command]

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,29 @@ int main() {
     std::cout << "$ ";
     
     std::getline(std::cin, command);
+    std::string builtIn[3] = {"echo", "type", "exit"};
 
-    std::string prefix = "echo";
     if(command == "exit"){
       return 0;
     }
-    else if(command.rfind(prefix, 0) == 0){
+    else if(command.rfind("echo", 0) == 0){
       std::string text = command.substr(5, command.size());
       std::cout << text << "\n";
+      continue;
+    }
+    else if(command.rfind("type", 0) == 0){
+      std::string text = command.substr(5,command.length());
+      bool found = false;
+      for(auto item : builtIn){
+        if(text == item){
+          std::cout << text << " is a shell builtin" << '\n';
+          found = true;
+          break;
+        }
+      }
+      if(!found){
+        std::cout << text << ": not found" << '\n';
+      }
       continue;
     }
     std::cout << command << ": command not found" << std::endl;
